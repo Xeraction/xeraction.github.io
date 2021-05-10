bddef16988196d74df4cf05df1ace3a4b2b35394
englishprojectforschool <83666204+englishprojectforschool@users.noreply.github.com>
2021-05-10 10:35:07 +0200

    Fancy generate stuff

---
+++ js/main.js
@@ -0,0 +1,30 @@
+function generate() {
+    const sentences = [
+        "You will see it soon.",
+        "It's coming.",
+        "It's inevitable.",
+        "You cannot stop it.",
+        "It may end you.",
+        "It has too much power.",
+        "You don't see it coming.",
+        "It's too dangerous.",
+        "It's too great of a force.",
+        "It cannot be destroyed.",
+        "The clock is ticking.",
+        "It's too great for you.",
+        "It has too much responsibility.",
+        "It's feared by everyone.",
+        "You cannot feel it.",
+        "It's the foundation of understanding.",
+        "We need it.",
+        "It will be beautiful.",
+        "Its widsom will be our demise.",
+        "It will take care of you.",
+        "You have to let it pass.",
+        "It will rule society.",
+        "It will decide your fate.",
+        "You will se its magnificence.",
+        "It will be done... soon."
+    ];
+    document.body.append(sentences[Math.floor(Math.random() * sentences.length)]);
+}
